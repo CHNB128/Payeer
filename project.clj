685b6a3a944d4db4d27f31be959547889ca4ce67
Daren <drake.aren@protonmail.com>
2019-10-09 13:16:25 +0600

    change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject payeer "0.1.0"
+(defproject payeer-clj "0.1.0"
   :description "Payeer api for clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
