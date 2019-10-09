81b5ef02b9e8aae478e761f71a1cfd5087468c98
Daren <drake.aren@protonmail.com>
2019-10-09 13:19:50 +0600

    add url ti project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject payeer-clj "0.1.0"
   :description "Payeer api wraper for clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/CHNB128/Payeer-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
