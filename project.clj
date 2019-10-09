23a107861f80f63ae8684f490c065bd5b3174919
Daren <drake.aren@protonmail.com>
2019-10-09 13:17:39 +0600

    change description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject payeer-clj "0.1.0"
-  :description "Payeer api for clojure"
+  :description "Payeer api wraper for clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
