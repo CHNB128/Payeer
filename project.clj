b336f8ec9e586a46f33aacfbd9cafdd8a6b5f71b
Daren <drake.aren@protonmail.com>
2019-10-11 12:25:49 +0600

    add url-utils to deps

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [digest "1.4.9"]
-                 [http-kit "2.3.0"]]
+                 [http-kit "2.3.0"]
+                 [url-utils "0.0.1"]]
   :repl-options {:init-ns payeer.core})
