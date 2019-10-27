81382f13d66b3a652fe99e83a28583cf9a740f32
drake.aren <drake.aren@protonmail.com>
2019-10-28 04:07:36 +0600

    increment project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject payeer "0.1.1"
+(defproject payeer "0.2.0"
   :description "Payeer api wraper for clojure"
   :url "https://github.com/CHNB128/Payeer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -7,5 +7,4 @@
                  [org.clojure/data.json "0.2.6"]
                  [digest "1.4.9"]
                  [http-kit "2.3.0"]
-                 [url-utils "0.0.1"]]
-  :repl-options {:init-ns payeer.core})
+                 [url-utils "0.0.1"]])
